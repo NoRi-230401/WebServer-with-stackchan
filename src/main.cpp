4ca31db2a053b4676af03f6f26314ce59244e231
NoRi-230401 <sedia2011-apple@yahoo.co.jp>
2023-12-20 22:23:00 +0900

    302e

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 // ---- <main.cpp>-------------------------------------------------------
 #include "main.h"
-const String WS_VERSION = "WebServer-with-stackchan_V302-230919";
+const String WS_VERSION = "WebServer-with-stackchan_V302-231222";
 
 // ---------------------------------------------------------------------
 // Extended from
